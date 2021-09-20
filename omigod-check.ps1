2b2f9d383d5dc5a453261dead5ee011a2d86c94d
Spencer Barnes-Israel <spencer.barnes-israel@us.qbe.com>
2021-09-20 16:24:21 -0400

    Remove printing tags, use Write-Output instead of Write-Host

--- omigod-check.ps1
+++ omigod-check.ps1
@@ -1,10 +1,6 @@
-$PrintWithTags = "true"
-
 $subs = Get-AzSubscription
 foreach($sub in $subs) {
-    Write-Host -ForegroundColor "cyan" -Message "=== Checking VMs in $($sub.name) ==="
     Set-AzContext -SubscriptionId $sub.Id
-
     $RGs = Get-AzResourceGroup
     foreach ($rg in $RGs) {
         $VMs = Get-AzVM -ResourceGroup $rg.ResourceGroupName
@@ -12,13 +8,7 @@ foreach($sub in $subs) {
             $exts = (Get-AzVM -ResourceGroupName $rg.ResourceGroupName -Name $vm.Name).Extensions
             foreach ($ext in $exts) {
                 if ($ext.VirtualMachineExtensionType -eq "OmsAgentForLinux") {
-                    Write-Host -ForegroundColor "Cyan" -Message "OmsAgent FOUND!! - $($vm.Name) with tags:"
-                    if ($PrintWithTags) {
-                        $tags = $vm.Tags
-                        $tags.GetEnumerator() | ForEach-Object {
-                            Write-Host -ForegroundColor "Cyan" -Message "$($_.key): $($_.value)"
-                        }
-                    }
+                    Write-Output "$($vm.Name), $($sub.name), $($sub.Id)"
                 }
             }
         }
